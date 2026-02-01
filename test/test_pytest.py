8498c5d95bfbc84fcc5c0fb70741378962ccb4d3
Fatima Zehrah <zehrah2010@gmail.com>
2026-02-01 16:44:47 -0500

    Step 4: add pytest and unittest tests

---
+++ test/test_pytest.py
@@ -0,0 +1,17 @@
+from src.calculator import fun1, fun2, fun3, fun4
+
+
+def test_fun1():
+    assert fun1(2, 3) == 5
+
+
+def test_fun2():
+    assert fun2(10, 4) == 6
+
+
+def test_fun3():
+    assert fun3(3, 5) == 15
+
+
+def test_fun4():
+    assert fun4(2, 3) == (2 + 3) + (2 - 3) + (2 * 3)
